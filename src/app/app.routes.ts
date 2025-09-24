0009517bbb01b2e333e3c24d5f6abf4c89f4e8a3
Felix <felyrics3@gmail.com>
2025-09-24 17:56:20 -0500

    Añade nuevas rutas y componentes para la gestión de categorías, talleres, festivales, servicios e inscripciones en el panel de administración; actualiza el diseño de la barra lateral.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,12 @@ import { Login } from './features/auth/login/login';
 import { Dashboard } from './features/pages/dashboard/dashboard';
 import { AuthGuard } from './core/guards/auth.guard';
 import { PanelAdmin } from './features/pages/panel-admin/panel-admin';
+import { PanelAdministracion } from './features/admin/panel-administracion/panel-administracion';
+import { Categoria } from './features/admin/categoria/categoria';
+import { Talleres } from './features/admin/talleres/talleres';
+import { Festivales } from './features/admin/festivales/festivales';
+import { Servicios } from './features/admin/servicios/servicios';
+import { Inscripciones } from './features/admin/inscripciones/inscripciones';
 
 export const routes: Routes = [
     {
@@ -12,7 +18,27 @@ export const routes: Routes = [
         path: 'login', component: Login
     },
     {
-        path: 'panel', component: PanelAdmin, canActivate: [AuthGuard]
+        path: 'panel', component: PanelAdmin, canActivate: [AuthGuard],
+        children: [
+            {
+                path: 'panel-administracion',component: PanelAdministracion
+            },
+            {
+                path: 'categoria', component:Categoria
+            },
+            {
+                path: 'talleres', component: Talleres
+            },
+            {
+                path: 'festivales', component: Festivales
+            },
+            {
+                path: 'servicios', component: Servicios
+            },
+            {
+                path: 'inscripciones', component: Inscripciones
+            }
+        ]
     },
 
     {
--- src/app/shared/components/left-sidebar/left-sidebar.ts
+++ src/app/shared/components/left-sidebar/left-sidebar.ts
@@ -12,28 +12,38 @@ import { MatIconModule } from '@angular/material/icon';
 export class LeftSidebar {
   isLeftSidebarCollapsed = input.required<boolean>();
   changeIsLeftSidebarCollapsed = output<boolean>();
-  items = [
+items = [
+  {
+    routeLink: 'panel-administracion',
+    icon: 'home',
+    label: 'Panel de Administración',
+  },
+  {
+    routeLink: 'categoria',
+    icon: 'category',
+    label: 'Gestion de Categoria y Subcategoria',
+  },
+  {
+    routeLink: 'talleres',
+    icon: 'article',
+    label: 'Gestion de Talleres',
+  },
+  {
+    routeLink: 'festivales',
+    icon: 'celebration',
+    label: 'Gestion de Festivales y Premios',
+  },
     {
-      routeLink: 'dashboard',
-      icon: 'fal fa-home',
-      label: 'Dashboard',
-    },
+    routeLink: 'servicios',
+    icon: 'build',
+    label: 'Gestion de Servicios',
+  },
     {
-      routeLink: 'products',
-      icon: 'fal fa-box-open',
-      label: 'Products',
-    },
-    {
-      routeLink: 'pages',
-      icon: 'fal fa-file',
-      label: 'Pages',
-    },
-    {
-      routeLink: 'settings',
-      icon: 'fal fa-cog',
-      label: 'Settings',
-    },
-  ];
+    routeLink: 'inscripciones',
+    icon: 'how_to_reg',
+    label: 'Gestion de Inscripciones',
+  },
+];
 
   toggleCollapse(): void {
     this.changeIsLeftSidebarCollapsed.emit(!this.isLeftSidebarCollapsed());
