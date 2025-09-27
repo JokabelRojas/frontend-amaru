8fc01be69975b660792a466bc3767b8ff9dfd4b4
Felix <felyrics3@gmail.com>
2025-09-27 12:45:44 -0500

    Añade encabezado y botones para la gestión de categorías y subcategorías; integra MatIconModule en el componente.

--- src/app/features/admin/categoria/categoria.ts
+++ src/app/features/admin/categoria/categoria.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-categoria',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './categoria.html',
   styleUrl: './categoria.css'
 })
