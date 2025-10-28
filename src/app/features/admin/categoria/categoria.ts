e353fd2f6ac7ac1968ef2507e20df3562253b38b
Felix <felyrics3@gmail.com>
2025-10-27 21:43:26 -0500

    Añade gestión de categorías y subcategorías con contenido dinámico; implementa navegación entre pestañas y tablas para visualización.

--- src/app/features/admin/categoria/categoria.ts
+++ src/app/features/admin/categoria/categoria.ts
@@ -1,12 +1,50 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-categoria',
-  imports: [MatIconModule],
+  imports: [MatIconModule, CommonModule ],
   templateUrl: './categoria.html',
   styleUrl: './categoria.css'
 })
 export class Categoria {
+activeTab: 'categorias' | 'subcategorias' = 'categorias';
+
+  categorias = [
+    {
+      id: 1,
+      nombre: 'Electrónica',
+      tipo: 'Principal',
+      descripcion: 'Dispositivos tecnológicos y gadgets.',
+      estado: 'Activo',
+      fechaCreacion: '2025-09-14',
+    },
+    {
+      id: 2,
+      nombre: 'Hogar',
+      tipo: 'Secundario',
+      descripcion: 'Artículos para el hogar y cocina.',
+      estado: 'Inactivo',
+      fechaCreacion: '2025-08-10',
+    },
+  ];
+
+  subcategorias = [
+    {
+      id: 1,
+      categoria: 'Electrónica',
+      descripcion: 'Teléfonos móviles y accesorios.',
+      estado: 'Activo',
+      fechaCreacion: '2025-09-20',
+    },
+    {
+      id: 2,
+      categoria: 'Hogar',
+      descripcion: 'Electrodomésticos pequeños.',
+      estado: 'Inactivo',
+      fechaCreacion: '2025-09-25',
+    },
+  ];
 
 }
